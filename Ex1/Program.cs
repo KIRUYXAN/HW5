65f4ba6b7330a5afbf180c468cf593c63a16f88c
KIRUYXAN <279901@gmail.com>
2022-01-12 00:04:41 +0300

    .

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -5,23 +5,56 @@
 //б) с использованием регулярных выражений.
 using System.Text.RegularExpressions;
 
-Regex Rus;
-Regex Num;
-Regex MinMax;
-string log;
-do
+Console.WriteLine("NReg или Reg");
+string a = "Reg"; //Console.ReadLine();
+if (a == "NReg" || a == "nreg")
+    if (NReg()) Console.WriteLine("\nЛогин корректен");
+
+if (a == "Reg" || a == "reg")
+    if (Reg()) Console.WriteLine("\nЛогин корректен");
+
+
+
+    bool NReg()
 {
-    Console.WriteLine("Введите логин");
-    log = Console.ReadLine();
-    Rus = new Regex(@"[^A-Za-z0-9]");
-    Num = new Regex(@"[0-9] + [A-Za-z0-9]");
-    MinMax = new Regex(@"[A-Za-z0-9]{2, 10}");
-    Console.WriteLine(Num.IsMatch(log));
-    Console.WriteLine(MinMax.IsMatch(log));
-    if (Rus.IsMatch(log)) Console.WriteLine("Логин может содержать только буквы латинского алфавита или цифры\n");
-    else if (Num.IsMatch(log)) Console.WriteLine("Превый символ не может быть цыфрой");
-    else if (MinMax.IsMatch(log)) Console.WriteLine("В логине должно быть от 2 до 10 символов");
+    string log = "";
+    int f;
+    int l;
+    do
+    {
+        Console.WriteLine("Введите логин");
+        log = Console.ReadLine();
+        f = Convert.ToInt32(log[0]);
+        l = log.Length;
+        //if () Console.WriteLine("Логин может содержать только буквы латинского алфавита или цифры\n");
 
-} while (Rus.IsMatch(log) || Num.IsMatch(log) || MinMax.IsMatch(log));
+       //if () Console.WriteLine("Превый символ не может быть цыфрой\n");
 
-Console.WriteLine("ДА");
+        if (l < 2 || 10 < l) Console.WriteLine("В логине должно быть от 2 до 10 символов\n");
+
+    } while ((l < 2 || 10 < l));
+    return true;
+}
+bool Reg()
+{
+    Regex EngNum;
+    Regex FNum;
+    Regex MinMax;
+    string log;
+    string f;
+    do
+    {
+        Console.WriteLine("Введите логин");
+        log = Console.ReadLine();
+        f = Convert.ToString(log[0]);
+        EngNum = new Regex(@"[^A-Za-z0-9]");
+        FNum = new Regex(@"[0-9]");
+        MinMax = new Regex("[A-Za-z0-9]{2,10}");
+        if (EngNum.IsMatch(log)) Console.WriteLine("Логин может содержать только буквы латинского алфавита или цифры\n");
+        else if (FNum.IsMatch(f)) Console.WriteLine("Превый символ не может быть цыфрой\n");
+        else if (!MinMax.IsMatch(log)) Console.WriteLine("В логине должно быть от 2 до 10 символов\n");
+
+    } while (EngNum.IsMatch(log) || FNum.IsMatch(f) || !MinMax.IsMatch(log));
+    return true;
+}
+
