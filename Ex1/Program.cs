21a34d5c1d7af122869b248ffc88f5a14b94c171
KIRUYXAN <279901@gmail.com>
2022-01-11 19:17:35 +0300

    .

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -3,3 +3,25 @@
 //содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
 //а) без использования регулярных выражений;
 //б) с использованием регулярных выражений.
+using System.Text.RegularExpressions;
+
+Regex Rus;
+Regex Num;
+Regex MinMax;
+string log;
+do
+{
+    Console.WriteLine("Введите логин");
+    log = Console.ReadLine();
+    Rus = new Regex(@"[^A-Za-z0-9]");
+    Num = new Regex(@"[0-9] + [A-Za-z0-9]");
+    MinMax = new Regex(@"[A-Za-z0-9]{2, 10}");
+    Console.WriteLine(Num.IsMatch(log));
+    Console.WriteLine(MinMax.IsMatch(log));
+    if (Rus.IsMatch(log)) Console.WriteLine("Логин может содержать только буквы латинского алфавита или цифры\n");
+    else if (Num.IsMatch(log)) Console.WriteLine("Превый символ не может быть цыфрой");
+    else if (!MinMax.IsMatch(log)) Console.WriteLine("В логине должно быть от 2 до 10 символов");
+
+} while (Rus.IsMatch(log) || Num.IsMatch(log) || !MinMax.IsMatch(log));
+
+Console.WriteLine("ДА");
